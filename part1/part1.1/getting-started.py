85795087e965b3d412ee1e830f3f1f12c2ced76d
esraoncu <168474793+eesraoncu@users.noreply.github.com>
2025-06-07 22:38:33 +0300

    Update getting-started.py

--- part1/part1.1/getting-started.py
+++ part1/part1.1/getting-started.py
@@ -1,7 +1,7 @@
-**Question1: Please write a program which prints out an emoticon: :-)**
+Question1: Please write a program which prints out an emoticon: :-)
 print(":-")
 
-**Question2: This program is supposed to print out the names of the brothers in alphabetical order, but it's not working quite right yet. Please fix the program so that the names are printed in the correct order.**
+Question2: This program is supposed to print out the names of the brothers in alphabetical order, but it's not working quite right yet. Please fix the program so that the names are printed in the correct order.
 *Wrong state:*                             *Correct state:*
 print("Simeoni")                           print("Aapo")
 print("Juhani")                            print("Eero")
@@ -10,3 +10,15 @@ print("Lauri")                             print("Lauri")
 print("Aapo")                              print("Simeoni")
 print("Tuomas")                            print("Timo")
 print("Timo")                              print("Tuomas")
+
+Question3: Please write a program which prints out the following lines exactly as they are written here, punctuation and all:                                          print("
+Row, row, row your boat,                          print("Row, row, row your boat,")
+Gently down the stream.              -------->    print("Gently down the stream.")
+Merrily, merrily, merrily, merrily,               print("Merrily, merrily, merrily, merrily,")
+Life is but a dream.                              print("Life is but a dream.")
+
+Question4: Please write a program which prints out the number of minutes in a year.
+print("Number of minutes in a year: ", 60*24*365)
+
+Question5: Please write a program which prints out the following:
+print("Hello there!") ----> print('print("Hello there!")')
