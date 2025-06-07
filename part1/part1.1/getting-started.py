35df9de2c6500da9ebd6a3ee80521b5bf648813e
esraoncu <168474793+eesraoncu@users.noreply.github.com>
2025-06-07 22:26:57 +0300

    Create getting-started.py

---
+++ part1/part1.1/getting-started.py
@@ -0,0 +1,12 @@
+**Question1: Please write a program which prints out an emoticon: :-)**
+print(":-")
+
+**Question2: This program is supposed to print out the names of the brothers in alphabetical order, but it's not working quite right yet. Please fix the program so that the names are printed in the correct order.**
+*Wrong state:*                             *Correct state:*
+print("Simeoni")                           print("Aapo")
+print("Juhani")                            print("Eero")
+print("Eero")                              print("Juhani")
+print("Lauri")                             print("Lauri")
+print("Aapo")                              print("Simeoni")
+print("Tuomas")                            print("Timo")
+print("Timo")                              print("Tuomas")
